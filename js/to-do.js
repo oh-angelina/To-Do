783788d85c10a45ee33cf4f2132c409f15666d13
ohangelina <ohangelina@users.noreply.github.com>
2019-12-23 16:41:27 -0500

    Addition of add and remove capabilities

--- js/to-do.js
+++ js/to-do.js
@@ -1,13 +1,51 @@
+// Select document elements
 const clear = document.getElementById("clear"); 
 const list = document.getElementById("list");
 const input = document.getElementById("input"); 
 
-// const check = "checked class name"
-// const uncheck = "uncheck class name"
+// Create an array for the list items
+var toDoList = [{},{}];
+//initialize id 
+let id = 0;
 
+
+function addToDo(toDo, id, checked, trash) {
+	
+	if(trash) {return; }
+
+	const text = `<li class ="list-item">
+					<label class = "container">
+					<p class = "task">${toDo}</p>
+					<input type="checkbox" name="complete" job="complete" id="${id}">
+					<span class="checkmark"></span>
+					</label>
+
+					<button class="remove" job="delete" id="${id}">x</button>	
+				</li>`; 
+
+	const position = "beforeend";
+
+	list.insertAdjacentHTML(position, text); 
+}
+
+document.addEventListener("keyup", function(event) {
+	if(event.keyCode == 13) {
+		const toDo = input.value;
+		//if not empty, add to do
+		if (toDo) {
+			addToDo(toDo); 
+		}
+		//Empties input after submit 
+		input.value = "";
+	}
+}); 
+
+addToDo("sleep", 1, false, false,)
+
+// On click, update or remove function will run on list item
 list.addEventListener("click", function(event){
 	let element = event.target; 
-	const elementJob = event.target.attribute.job.value;
+	const elementJob = event.target.attributes.job.value;
 		
 		if(elementJob == "complete") {
 			completeToDo(element);
@@ -18,73 +56,17 @@ list.addEventListener("click", function(event){
 
 
 function completeToDo (element) {
-	element.classList.toggle(checked); 
+	element.classList.toggle("checked"); 
+	//list[element.id].checked = list[element.id].checked ? true : false;
 
-	list[element.id].checked = list[element.id].checked ? true : false; 
 }
 
 function removeToDo (element) {
 	element.parentNode.parentNode.removeChild(element.parentNode); 
-	list[element.id].trash = true; 
+	//list[element.id].trash = true; 
 }
 
-
-function addToDo(toDo, id, checked, trash) {
-
-// Need to find a way to see if the list item that is selected is checked or not
-// then can remove/rename the done code
-	
-	if(trash) {return; }
-	const Done = element.checked ? true : false;
-
-	const text = `<li class ="list-item">
-					<label class = "container">
-					<p class = "task">${toDo}</p>
-					<input type="checkbox" name="complete" job="complete">
-					<span class="checkmark"></span>
-					</label>
-
-					<button class="remove" job="delete">x</button>	
-				</li>`; 
-
-	const position = "beforeend";
-
-	list.insertAdjacentHTML(position, text); 
-}
-
-document.addEventListener("keyup",function(event) {
-	if(event.keycode == 13) {
-		const toDo = input.value;
-		if(toDo) {
-			addToDo(toDo, id, false, false);
-			toDoList.push (
-				{
-					name: toDo,
-					id: id,
-					checked: false,
-					trash: false, 
-				}
-			)
-		}
-		input.value=""; 
-		id++;
-	}
-})
-
-var toDoList = [];
-let id = 0;
-
-toDoList = [
-	{
-		name: "Drink Coffee",
-		id: 0,
-		checked : false, 
-		trash : false
-	},
-	{
-		name: "Work out",
-		id: 1, 
-		checked : false,
-		trash : false, 
-	}
-]; 
+clear.addEventListener("click", function() {
+	localSorage.cler(); 
+	location.reload();
+});
